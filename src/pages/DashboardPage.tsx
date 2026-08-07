de4c259ede1410323def3af0f14a623dfb2947ba
daamiir <toleukhanovdamir2005@gmail.com>
2026-08-07 17:58:54 +0500

    fix: max main container width 1024px

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -9,32 +9,34 @@ import { Users, Box } from "lucide-react";
 
 const DashboardPage = () => {
   return (
-    <div className="grid grid-cols-12 gap-4 items-stretch md:gap-6">
-      <div className="col-span-12 space-y-6 xl:col-span-7">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
-          <DashboardCard
-            icon={<Users />}
-            title="Customers"
-            value="3782"
-            change="+11.1%"
-          />
-          <DashboardCard
-            icon={<Box />}
-            title="Orders"
-            value="5359"
-            change="-9.05%"
-          />
+    <div className="max-w-5xl mx-auto space-y-6">
+      <div className="grid grid-cols-12 gap-4 items-stretch md:gap-6">
+        <div className="col-span-12 space-y-6 xl:col-span-7">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+            <DashboardCard
+              icon={<Users />}
+              title="Customers"
+              value="3782"
+              change="+11.1%"
+            />
+            <DashboardCard
+              icon={<Box />}
+              title="Orders"
+              value="5359"
+              change="-9.05%"
+            />
+          </div>
+          <DashboardBarChart />
         </div>
-        <DashboardBarChart />
-      </div>
-      <div className="col-span-12 flex flex-col xl:col-span-5">
-        <DashboardPieChart />
-      </div>
-      <div className="col-span-12 xl:col-span-5">
-        <CustomerDemographics />
-      </div>
-      <div className="col-span-12 xl:col-span-7">
-        <RecentOrders />
+        <div className="col-span-12 flex flex-col xl:col-span-5">
+          <DashboardPieChart />
+        </div>
+        <div className="col-span-12 xl:col-span-5">
+          <CustomerDemographics />
+        </div>
+        <div className="col-span-12 xl:col-span-7">
+          <RecentOrders />
+        </div>
       </div>
     </div>
   );
