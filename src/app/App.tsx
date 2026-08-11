554a711001a902f7e7bd2fe6119bc4b03e06cf80
daamiir <toleukhanovdamir2005@gmail.com>
2026-08-11 18:34:45 +0500

    add: dark mode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
 import { AppProviders } from "./AppProviders";
 import AppLayout  from "@/components/layout/AppLayout";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 export default function App() {
   return (
-    <AppProviders>
-      <AppLayout>
-        <AppRoutes />
-      </AppLayout>
-    </AppProviders>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AppProviders>
+        <AppLayout>
+          <AppRoutes />
+        </AppLayout>
+      </AppProviders>
+    </ThemeProvider>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,15 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 flex h-16 w-full items-center justify-between gap-4 border-b bg-background px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-      <div className="flex items-center gap-2">
+    <header className="sticky top-0 z-50 flex h-16 w-full items-center gap-4 border-b bg-background px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <div className="flex justify-between w-full">
         <SidebarTrigger className="-ml-1" />
+        <ModeToggle />
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
